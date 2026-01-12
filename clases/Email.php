caf05874c73faf8f452933167432a1ca6633dfa9
Fabian Peña <fabianpena@iMac-de-Fabian.local>
2026-01-12 13:06:35 -0500

    Actualiza envío de emails con variables de entorno

--- clases/Email.php
+++ clases/Email.php
@@ -33,7 +33,7 @@ class Email
         $mail->Timeout = 30;
 
         // FROM debe ser el mismo correo de Gmail
-        $mail->setFrom($_ENV['EMAIL_USER'], 'Tendencia Peluqueria');
+        $mail->setFrom('challo2341@gmail.com', 'Tendencia Peluqueria');
     }
 
     public function enviarConfirmacion()
